d9418bb86a874583ff2dbbca01e6f0068e6f0553
CavanLeoJacobs <cavanleojacobs@gmail.com>
2021-10-07 15:45:49 +0200

    Initial commit

---
+++ ProjectForAccountSystemSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/RestServiceApplication.java
@@ -0,0 +1,16 @@
+package za.ac.nwu.ac.web.sb;
+
+//import org.slf4.Logger;
+//import org.slf4.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication
+{
+    public static void main(String[]args)
+    {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+
+}
